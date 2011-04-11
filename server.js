b7bc28ab79bad7d9a1ba98d1562d5025c77e4b54
fudder <mail@cardographer.com>
2011-04-11 13:55:43 -0700

    DB integrated

--- server.js
+++ server.js
@@ -16,60 +16,21 @@ app.resource = function(path, obj) {
     obj.range(req, res, a, b, format);
   });
   this.get(path + '/:id', obj.show);
-  this.del(path + '/:id', obj.destroy);
+//this.del(path + '/:id', obj.destroy);
 };
 
-// Fake records
-
-var users = [
-    { name: 'tj' }
-  , { name: 'ciaran' }
-  , { name: 'aaron' }
-  , { name: 'guillermo' }
-  , { name: 'simon' }
-  , { name: 'tobi' }
-];
-
+// Data
 var newsItems = [];
 
 db.open("news.db", function (error) {
   var sql = 'SELECT * FROM news';
   db.execute(sql, function (error, rows) {
     newsItems = rows;
+    console.log(rows.length + ' news items loaded from db.');
   });
 });
 
-// Fake controllers
-
-var User = {
-  index: function(req, res){
-    res.send(users);
-  },
-  show: function(req, res){
-    res.send(users[req.params.id] || { error: 'Cannot find user' });
-  },
-  destroy: function(req, res){
-    var id = req.params.id;
-    var destroyed = id in users;
-    delete users[id];
-    res.send(destroyed ? 'destroyed' : 'Cannot find user');
-  },
-  range: function(req, res, a, b, format){
-    var range = users.slice(a, b + 1);
-    switch (format) {
-      case 'json':
-        res.send(range);
-        break;
-      case 'html':
-      default:
-        var html = '<ul>' + range.map(function(user){
-          return '<li>' + user.name + '</li>';
-        }).join('\n') + '</ul>';
-        res.send(html);
-        break;
-    }
-  }
-};
+// Controllers
 
 var News = {
   index: function(req, res){
@@ -101,25 +62,12 @@ var News = {
   }
 };
 
-
-// curl http://localhost:3000/users     -- responds with all users
-// curl http://localhost:3000/users/1   -- responds with user 1
-// curl http://localhost:3000/users/4   -- responds with error
-// curl http://localhost:3000/users/1..3 -- responds with several users
-// curl -X DELETE http://localhost:3000/users/1  -- deletes the user
-
-app.resource('/users', User);
 app.resource('/news', News);
 
 app.get('/', function(req, res){
   res.send([
-      '<h1>Examples:</h1> <ul>'
-    , '<li>GET /users</li>'
-    , '<li>GET /users/1</li>'
-    , '<li>GET /users/3</li>'
-    , '<li>GET /users/1..3</li>'
-    , '<li>GET /users/1..3.json</li>'
-    , '<li>DELETE /users/4</li>'
+      '<h1>Services:</h1> <ul>'
+    , '<li>GET /news</li>'
     , '</ul>'
   ].join('\n')); 
 });
